da1dafd7a5bce842b3d1e5542625bd86ea5d7b3e
Lex <lexthemenace@gmail.com>
2020-07-28 22:25:40 -0500

    w/ passport edit

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
-var mysql = require("mysql");
+const mysql = require("mysql");
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "us-cdbr-east-02.cleardb.com",
   port: 3306,
   user: "b7258238403bd1",
@@ -8,7 +8,7 @@ var connection = mysql.createConnection({
   database: "heroku_d17f969daa1a53a"
 });
 
-connection.connect(function (err) {
+connection.connect(err => {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
@@ -16,26 +16,4 @@ connection.connect(function (err) {
   console.log("connected as id " + connection.threadId);
 });
 
-function handleDisconnect() {
-  connection = mysql.createConnection(db_config); // Recreate the connection, since
-                                                  // the old one cannot be reused.
-
-  connection.connect(function(err) {              // The server is either down
-    if(err) {                                     // or restarting (takes a while sometimes).
-      console.log('error when connecting to db:', err);
-      setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
-    }                                     // to avoid a hot loop, and to allow our node script to
-  });                                     // process asynchronous requests in the meantime.
-                                          // If you're also serving http, display a 503 error.
-  connection.on('error', function(err) {
-    console.log('db error', err);
-    if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
-      handleDisconnect();                         // lost due to either server restart, or a
-    } else {                                      // connnection idle timeout (the wait_timeout
-      throw err;                                  // server variable configures this)
-    }
-  });
-}
-
-handleDisconnect();
 module.exports = connection;
--- config/orm.js
+++ config/orm.js
@@ -1,18 +1,18 @@
-var connection = require("./connection");
+const connection = require("./connection");
 function printQuestionMarks(num) {
-  var arr = [];
+  const arr = [];
 
-  for (var i = 0; i < num; i++) {
+  for (let i = 0; i < num; i++) {
     arr.push("?");
   }
   return arr.toString();
 }
 
 function objToSql(ob) {
-  var arr = [];
+  const arr = [];
 
-  for (var key in ob) {
-    var value = ob[key];
+  for (const key in ob) {
+    let value = ob[key];
 
     if (Object.hasOwnProperty.call(ob, key)) {
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
@@ -24,18 +24,18 @@ function objToSql(ob) {
   return arr.toString();
 }
 
-var orm = {
-  selectAll: function (tableInput, cb) {
-    var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function (err, result) {
+const orm = {
+  selectAll: function(tableInput, cb) {
+    const queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, (err, result) => {
       if (err) {
         throw err;
       }
       cb(result);
     });
   },
-  insertOne: function (table, cols, vals, cb) {
-    var queryString = "INSERT INTO " + table;
+  insertOne: function(table, cols, vals, cb) {
+    let queryString = "INSERT INTO " + table;
 
     queryString += " (";
     queryString += cols.toString();
@@ -46,7 +46,7 @@ var orm = {
 
     console.log(queryString);
 
-    connection.query(queryString, vals, function (err, result) {
+    connection.query(queryString, vals, (err, result) => {
       if (err) {
         throw err;
       }
@@ -55,8 +55,8 @@ var orm = {
     });
   },
 
-  updateOne: function (table, objColVals, condition, cb) {
-    var queryString = "UPDATE " + table;
+  updateOne: function(table, objColVals, condition, cb) {
+    let queryString = "UPDATE " + table;
     //set __ where __
     queryString += " SET ";
     queryString += objToSql(objColVals);
@@ -64,7 +64,7 @@ var orm = {
     queryString += condition;
 
     console.log(queryString);
-    connection.query(queryString, function (err, result) {
+    connection.query(queryString, (err, result) => {
       if (err) {
         throw err;
       }
